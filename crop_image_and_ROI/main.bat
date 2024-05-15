485305342ecd3631c2a90cbfb6cdd6a885bd9be2
maosongran <maosongran@gmail.com>
2024-05-15 11:37:38 +0800

    fix main.bat bug: hide cmd windows

--- crop_image_and_ROI/main.bat
+++ crop_image_and_ROI/main.bat
@@ -1,2 +1,8 @@
+@echo off
+if "%1"=="h" goto begin
+start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+:begin
+
 CALL activate demo1
 python crop_image.py 
+pause
